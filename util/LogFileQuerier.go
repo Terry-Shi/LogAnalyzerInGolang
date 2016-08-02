bdf08c650c58759f4441591baa96aae75dac1fcd
Terry <terryemailbox@gmail.com>
2016-08-02 16:57:10 +0800

    generate map which key is eprid, value is request times.

--- util/LogFileQuerier.go
+++ util/LogFileQuerier.go
@@ -1,65 +1,88 @@
 package main
 
 import (
-	"os"
-	//"bufio"
-	"fmt"
-	"io/ioutil"
-	"path/filepath"
-	"strings"
 	"bufio"
 	"compress/gzip"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"strings"
+	"sort"
 )
 
 // ref:Golang-文件操作 http://www.nljb.net/default/Golang-%E6%96%87%E4%BB%B6%E6%93%8D%E4%BD%9C/
 
-// 1. 函数参数为可变数量
-// 2. 文件操作。 a.遍历某目录下所有文件 b. 读取文件内容
+//OK 1. 函数参数为可变数量
+//OK 2. 文件操作。 a.遍历某目录下所有文件 b. 读取文件内容
 // 3. 利用正则表达式过滤文件
-// 4. read gzip file directly
+//OK 4. read gzip file directly
+// 5 错误、异常处理
+// 6 排序 map http://blog.csdn.net/slvher/article/details/44779081
+
 func main() {
-	queryWithDateRange("C:\\WORK\\IDE\\IdeaProjects\\LogAnalyzerInGolang", "")
+	ret, err := queryWithDateRange("C:\\WORK\\IDE\\IdeaProjects\\LogAnalyzerInGolang\\logfile", "")
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println(ret)
+		sort.Sort()
+	}
 }
 
-func queryWithDateRange(path string, logdate string) map[string]string {
-	ret := make(map[string]string)
+func queryWithDateRange(path string, logdate ...string) (map[string]int, error) {
+	ret := make(map[string]int) // key: eprid, value: request times
 
-	//logpath,_ := os.Open(path)
-	//getFilelist(path)
-	fmt.Println(" ------ ")
-	files, err := ListDir(path, "md")
-	if (err != nil) {
+	files, err := ListDir(path, "gz")
+	if err != nil {
 		return ret
 	}
 	for _, file := range files {
-	    fmt.Println(file)
+		fmt.Println(file)
 		//打开文件，并进行相关处理
-		f , err := os.Open(file)
-		if err != nil{
-			fmt.Printf("%v\n",err)
-			os.Exit(1)
+		f, err := os.Open(file)
+		if err != nil {
+			fmt.Printf("%v\n", err)
+			break
+			//os.Exit(1)
 		}
 		//文件关闭 TODO: how to avoid leak ? put the code which inside the loop into a function
 		defer f.Close()
-		//将文件作为一个io.Reader对象进行buffered I/O操作
-		var br *bufio.Reader
-		if strings.HasSuffix(file, ".gz") {
-			br,_ = gzip.NewReader(f)
-		} else {
-		        br = bufio.NewReader(f)
-		}
-		for{
 
-			line , err := br.ReadString('\n')
-			if err == os.EOF {
-				break
-			}else{
-				fmt.Printf("%v",line)
+		var line string = ""
+		if strings.HasSuffix(file, ".gz") {
+			//将文件作为一个io.Reader对象进行buffered I/O操作
+			reader, _ := gzip.NewReader(f)
+			br := bufio.NewReader(reader)
+			for {
+				line, err = br.ReadString('\n')
+				if err != nil {
+					break
+				} else {
+					eprid := GetEprid(line)
+					ret[eprid] = ret[eprid] + 1
+				}
+			}
+		} else {
+			scanner := bufio.NewScanner(f)
+			for scanner.Scan() {
+				line = scanner.Text()
+				//fmt.Printf("%v",line)
+				eprid := GetEprid(line)
+				ret[eprid] = ret[eprid] + 1
 			}
 		}
-
 	}
 	return ret
+}
+
+func GetEprid(oneline string) (eprid string) {
+	idx := strings.Index(oneline, "/fez/")
+	if idx != -1 {
+		return oneline[idx+5 : idx+5+6]
+	} else {
+		return ""
+	}
 }
 
 // 获取指定目录下的所有文件，不进入下级目录搜索，可以匹配后缀过滤。
