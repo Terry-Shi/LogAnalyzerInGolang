a8da2e39e30cf2e6167acc360a5951624fd49d5c
Terry-Shi <terryemailbox@gmail.com>
2016-08-03 23:55:29 +0800

    avoid resource leak.

--- util/LogFileQuerier.go
+++ util/LogFileQuerier.go
@@ -19,6 +19,7 @@ import (
 //OK 4. read gzip file directly
 // 5 错误、异常处理
 // 6 排序 map http://blog.csdn.net/slvher/article/details/44779081
+// 7 best practice: clean code
 
 func main() {
 	ret, err := queryWithDateRange("C:\\WORK\\IDE\\IdeaProjects\\LogAnalyzerInGolang\\logfile", "")
@@ -26,10 +27,28 @@ func main() {
 		fmt.Println(err)
 	} else {
 		fmt.Println(ret)
+		// 根据key排序
+		sorted_keys := make([]string, 0)
+		for k, _ := range ret {
+			sorted_keys = append(sorted_keys, k)
+		}
+		// sort 'string' key in increasing order
+		sort.Strings(sorted_keys)
+		for _, k := range sorted_keys {
+			fmt.Printf("k=%v, v=%v\n", k, ret[k])
+		}
+
+		// TODO: 自定义数据结构排序, 以map的value从小到大排序
+		// http://www.kancloud.cn/itfanr/go-by-example/81648
+		//实现了sort接口的Len，Less和Swap方法这样我们就可以使用sort包的通用方法Sort
 		sort.Sort()
 	}
 }
 
+/**
+ path: log file path
+ logdate: which date's log you want to query
+ */
 func queryWithDateRange(path string, logdate ...string) (map[string]int, error) {
 	ret := make(map[string]int) // key: eprid, value: request times
 
@@ -37,41 +56,44 @@ func queryWithDateRange(path string, logdate ...string) (map[string]int, error) 
 	if err != nil {
 		return ret
 	}
-	for _, file := range files {
-		fmt.Println(file)
-		//打开文件，并进行相关处理
-		f, err := os.Open(file)
-		if err != nil {
-			fmt.Printf("%v\n", err)
-			break
-			//os.Exit(1)
-		}
-		//文件关闭 TODO: how to avoid leak ? put the code which inside the loop into a function
-		defer f.Close()
+	for _, fileFullPathName := range files {
+		fmt.Println(fileFullPathName)
 
-		var line string = ""
-		if strings.HasSuffix(file, ".gz") {
-			//将文件作为一个io.Reader对象进行buffered I/O操作
-			reader, _ := gzip.NewReader(f)
-			br := bufio.NewReader(reader)
-			for {
-				line, err = br.ReadString('\n')
-				if err != nil {
-					break
-				} else {
+		func() {
+			//打开文件，并进行相关处理
+			file, err := os.Open(fileFullPathName)
+			if err != nil {
+				fmt.Printf("%v\n", err)
+				return
+			}
+			//文件关闭
+			// DONE: how to avoid leak ? put the code which inside the loop into a function。用匿名函数解决
+			defer file.Close()
+
+			var line string = ""
+			if strings.HasSuffix(fileFullPathName, ".gz") {
+				//将文件作为一个io.Reader对象进行buffered I/O操作
+				reader, _ := gzip.NewReader(file)
+				br := bufio.NewReader(reader)
+				for {
+					line, err = br.ReadString('\n')
+					if err != nil {
+						break
+					} else {
+						eprid := GetEprid(line)
+						ret[eprid] = ret[eprid] + 1
+					}
+				}
+			} else {
+				scanner := bufio.NewScanner(file)
+				for scanner.Scan() {
+					line = scanner.Text()
+					//fmt.Printf("%v",line)
 					eprid := GetEprid(line)
 					ret[eprid] = ret[eprid] + 1
 				}
 			}
-		} else {
-			scanner := bufio.NewScanner(f)
-			for scanner.Scan() {
-				line = scanner.Text()
-				//fmt.Printf("%v",line)
-				eprid := GetEprid(line)
-				ret[eprid] = ret[eprid] + 1
-			}
-		}
+		}()
 	}
 	return ret
 }
@@ -85,10 +107,10 @@ func GetEprid(oneline string) (eprid string) {
 	}
 }
 
-// 获取指定目录下的所有文件，不进入下级目录搜索，可以匹配后缀过滤。
+// 获取指定目录下的所有文件，不进入下级目录搜索，可以通过匹配后缀过滤。
 func ListDir(dirPth string, suffix string) (files []string, err error) {
-	files = make([]string, 0, 10)
-	dir, err := ioutil.ReadDir(dirPth)
+	files = make([]string, 0, 10) // TODO: 含义:allocates a slice of length 0 and capacity 10.
+	dir, err := ioutil.ReadDir(dirPth) // TODO: 熟悉常用lib的功能 ioutil, os, fmt,  etc
 	if err != nil {
 		return nil, err
 	}
